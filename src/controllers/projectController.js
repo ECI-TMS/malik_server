8c847ef75da5239c44bb56475276186389901408
abdulmalikshakir2001 <abdulmalikshakir2001@gmail.com>
2025-05-26 00:20:18 +0500

    added link field in query for proudction / master branch

--- src/controllers/projectController.js
+++ src/controllers/projectController.js
@@ -340,7 +340,7 @@ export const getProjectWithImages = async (req, res, next) => {
     // Get all projects with their images
     const [projects] = await connection.query(`
       SELECT 
-        p.id AS project_id, p.title, p.description, p.image_path, 
+        p.id AS project_id, p.title,p.link, p.description, p.image_path, 
         pi.id AS image_id, pi.image_path AS project_image_path
       FROM projects p
       LEFT JOIN project_images pi ON p.id = pi.project_id
@@ -359,7 +359,7 @@ export const getProjectWithImages = async (req, res, next) => {
           title: row.title,
           description: row.description,
           image_path: row.image_path,
-          link: "", // Set this dynamically or add a link column in DB
+          link: row.link, 
           images: [],
         });
       }
